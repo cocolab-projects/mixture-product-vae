644d92377a56fc41d9728bae8756d283d263232a
mhw32 <me@mikewuis.me>
2019-06-18 18:00:52 -0700

    upgrad

--- baseline-multimodel-vae/main.py
+++ baseline-multimodel-vae/main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 from tqdm import tqdm
 from torch.utils import data
+import torch.nn.functional as F
 
 from pyro.distributions import MixtureOfDiagNormals
 
@@ -93,60 +94,72 @@ def gaussian_kl_divergence(mean: torch.Tensor, logvar: torch.Tensor, weight: flo
     return weight * kl_div
 
 
-def ELBO(x: torch.Tensor, reconstructed_mean, reconstructed_logvar,
-         latent_mean, latent_logvar) -> float:
+def ELBO(x: torch.Tensor, reconstructed_mean: torch.Tensor, reconstructed_logvar: torch.Tensor,
+         latent_mean: torch.Tensor, latent_logvar: torch.Tensor) -> float:
     pdf_loss = log_pdf(x, reconstructed_mean, reconstructed_logvar)
-    kl_loss = gaussian_kl_divergence(latent_mean, latent_logvar, weight=100)
+    kl_loss = gaussian_kl_divergence(latent_mean, latent_logvar, weight=1)
     return torch.mean(-pdf_loss - kl_loss)
 
 
 class BaselineVAE(torch.nn.Module):
-
-    def __init__(self, input_size, hidden_size, latent_size,
-                 activation: Callable = torch.tanh) -> None:
+    def __init__(self, input_size: int, hidden_size: int, latent_size: int) -> None:
         super().__init__()
-        self.activation = activation
         self.encoder_input_to_hidden = torch.nn.Linear(input_size, hidden_size)
         self.encoder_hidden_to_mean = torch.nn.Linear(hidden_size, latent_size)
         self.encoder_hidden_to_logvar = torch.nn.Linear(hidden_size, latent_size)
-        self.linear = torch.nn.Linear(2, 2)
-        self.distributions = torch.nn.Parameter(torch.randn(32, 2).uniform_())
-        self.component_logits = None
 
     def encode(self, x: torch.Tensor):
-        self.component_logits = self.linear(x)
-        hidden = self.activation(self.encoder_input_to_hidden(x))
         hidden = self.encoder_input_to_hidden(x)
         latent_mean = self.encoder_hidden_to_mean(hidden)
         latent_logvar = self.encoder_hidden_to_logvar(hidden)
         return latent_mean, latent_logvar
 
-    def decode(self, latent_mean: torch.Tensor,
-               latent_logvar: torch.Tensor) -> torch.Tensor:
-
-        reconstructed_mean = MixtureOfDiagNormals(
-            locs=latent_mean.view(-1, 2, 2),
-            coord_scale=(0.5 * latent_logvar.view(-1, 2, 2)).exp(),
-            component_logits=torch.nn.functional.softmax(
-                self.component_logits, dim=0)
-        ).rsample()
-
-        reconstructed_logvar = torch.log(torch.ones_like(reconstructed_mean) * 0.1)
-        reconstructed_logvar = reconstructed_logvar.to(latent_mean.device)
-        return reconstructed_mean, reconstructed_logvar
-
     def reparameterize(self, latent_mean: torch.Tensor,
                        latent_logvar: torch.Tensor) -> torch.Tensor:
         return reparameterization_trick(latent_mean, latent_logvar)
+
+    def decode(self, latent: torch.Tensor) -> torch.Tensor:
+        reconstructed_mean = latent
+        reconstructed_logvar = torch.log(torch.ones_like(reconstructed_mean) * 0.1)
+        reconstructed_logvar = reconstructed_logvar.to(latent_mean.device)
+        return reconstructed_mean, reconstructed_logvar
 
     def forward(self, x: torch.Tensor):
         latent_mean, latent_logvar = self.encode(x)
         latent = self.reparameterize(latent_mean, latent_logvar)
         reconstructed_mean, reconstructed_logvar = self.decode(latent)
-        return reconstructed_mean, reconstructed_logvar, latent, latent_mean, latent_logvar
+        return reconstructed_mean, reconstructed_logvar, latent_mean, latent_logvar
+
+
+class MixtureVAE(BaselineVAE):
+
+    def __init__(self, input_size: int, hidden_size: int, latent_size: int, mixture_size: int) -> None:
+        super().__init__(input_size, hidden_size, latent_size)
+        self.component_layer = torch.nn.Linear(input_size, mixture_size)
+        self.mixture_size = mixture_size
+        self.input_size = input_size
+
+    def reparameterize(self, latent_mean: torch.Tensor,
+                       latent_logvar: torch.Tensor,
+                       component_logits: torch.Tensor) -> torch.Tensor:
+        latent_sigma = torch.exp(0.5 * latent_logvar)
+        latent = MixtureOfDiagNormals(
+            locs=latent_mean.view(-1, self.mixture_size, self.input_size),
+            coord_scale=latent_sigma.view(-1, self.mixture_size, self.input_size),
+            component_logits=F.softmax(component_logits, dim=1),
+        ).rsample()
+        return latent
+
+    def forward(self, x: torch.Tensor):
+        latent_mean, latent_logvar = self.encode(x)
+        component_logits = self.component_layer(x)
+        latent = self.reparameterize(latent_mean, latent_logvar, component_logits)
+        reconstructed_mean, reconstructed_logvar = self.decode(latent)
+        return reconstructed_mean, reconstructed_logvar, latent_mean, latent_logvar
 
 
 def add_hyperparameters(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    parser.add_argument('--mixture', action='store_true', default=False)
     parser.add_argument('--epochs', default=100, type=int)
     parser.add_argument('--batch-size', default=32, type=int)
     parser.add_argument('--dataset-size', default=10000, type=int)
@@ -160,7 +173,11 @@ def main():
     parser = add_hyperparameters(parser)
     args = parser.parse_args()
 
-    model = BaselineVAE(input_size=2, hidden_size=32, latent_size=2)
+    if args.mixture:
+        model = MixtureVAE(input_size=2, hidden_size=32, latent_size=2, mixture_size=2)
+    else:
+        model = BaselineVAE(input_size=2, hidden_size=32, latent_size=2)
+
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
     dataset = SimpleMultimodelDataset(args.dataset_size)
