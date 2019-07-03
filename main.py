333ebfbd539cebdd602aa2a2b549c1401f64f7b8
Conner <connervercellino@gmail.com>
2019-07-03 16:34:17 -0700

    Split ignite imports

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import torch
 import torchvision
 
 from ignite.contrib.handlers import ProgressBar
-from ignite.engine import Engine, events
+from ignite.engine import Engine
+from ignite.engine import Events
 from ignite.handlers import ModelCheckpoint, Timer
 from ignite.metrics import RunningAverage
 from tqdm import tqdm
