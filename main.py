8779793f8cbea007355e5949f40c0ae988b53dbf
Conner <connervercellino@gmail.com>
2019-07-03 13:07:36 -0700

    Remove AverageMeter

--- main.py
+++ main.py
@@ -6,24 +6,6 @@ import torchvision
 from tqdm import tqdm
 
 
-class AverageMeter:
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
-
-
 def gen_32_conv_output_dim(s):
     s = get_conv_output_dim(s, 2, 0, 2)
     s = get_conv_output_dim(s, 2, 0, 2)
